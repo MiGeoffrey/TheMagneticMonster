141af3662fffd9ac22a66d8e256cd5035ab1a9f6
Hugo Trentesaux <hugo.trentesaux@u-psud.fr>
2019-04-10 14:24:33 +0200

    period

--- MotorControl.ino/MotorControl.ino.ino
+++ MotorControl.ino/MotorControl.ino.ino
@@ -4,7 +4,10 @@
 
 int pStep = 12;
 int pDir = 13;
-int period = 2000;
+float period;
+byte SerialOK=0;
+char str[10];
+int i;
 boolean isRunning = false;
 
 // === Setup =======================================================
@@ -20,13 +23,40 @@ void setup() {
   // --- Serial communication
   Serial.begin(115200);
   Serial.setTimeout(10);
-  
+
+  //saisie de la période
+  Serial.println("enter the period");
+  while(SerialOK==0){
+    if(Serial.available()>0){
+      delay(5);
+      for(i=0;i<10;i++) str[i]=NULL;
+        i=0;
+      while(Serial.available()){
+        str[i++]= Serial.read();
+        Serial.print(str[i]);
+      }
+      if(atof(str)!=10) SerialOK=1;
+    }
+      //Serial.print("period=");
+     // period = Serial.read();
+      //if(period != 0){
+      //  Serial.println("the period is");
+      //  Serial.println(period);
+      //  SerialOK=1;
+     // }else Serial.println("recommence");
+    //}
+ 
+  }
+  period = atof(str);
+  Serial.println("the period is");
+  Serial.println(period);
+  Serial.println("le programme est pret"); 
 }
 
 // === Main loop ===================================================
 
 void loop() {
-
+  
   // --- Manage inputs ---------------------------------------------
   
   if (Serial.available()) {   
